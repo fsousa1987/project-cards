cb9fabb90e1c4a1bb031e29d5746d2932cc88eb4
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-21 13:19:14 -0300

    Criação do Card de Cartão de Crédito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BalanceCardComponent} from './components/balance-card/balance-card.component';
 import {NgOptimizedImage} from '@angular/common';
+import {CreditCardComponent} from './components/credit-card/credit-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BalanceCardComponent
+    BalanceCardComponent,
+    CreditCardComponent
   ],
   imports: [
     BrowserModule,
