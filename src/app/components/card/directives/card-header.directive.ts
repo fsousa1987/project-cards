4d4c27b7751b879530728ebbcf790fa32296c0d6
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-26 12:49:23 -0300

    Primeira Solução para o Background do Header

--- src/app/components/card/directives/card-header.directive.ts
+++ src/app/components/card/directives/card-header.directive.ts
@@ -1,4 +1,4 @@
-import {Directive} from '@angular/core';
+import {Directive, HostBinding, Input} from '@angular/core';
 
 @Directive({
   selector: 'app-card-header',
@@ -6,4 +6,6 @@ import {Directive} from '@angular/core';
   standalone: false
 })
 export class CardHeaderDirective {
+  @Input()
+  @HostBinding('class') color: string = '';
 }
